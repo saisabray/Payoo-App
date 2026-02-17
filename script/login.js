a2c624f00cb2fc68447f711643aabc21312ae33f
saisabray <saisabray1402@gmail.com>
2026-02-18 00:32:49 +0600

    Added script to login page

---
+++ script/login.js
@@ -0,0 +1,10 @@
+document.getElementById("login").addEventListener("click", function () {
+  const number = document.getElementById("number").value;
+  const pass = document.getElementById("pass").value;
+  if (number == "01234567890" && pass == "1234") {
+    alert("Access granted");
+    window.location.assign("home.html");
+  } else {
+    alert("Login Failed.Try Again");
+  }
+});
